03c9419d908a18c0652c082fbc95e3fa78f69631
d.kanellidis@msensis.com <d.kanellidis@msensis.com>
2022-07-05 10:09:02 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by MSENSIS on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
