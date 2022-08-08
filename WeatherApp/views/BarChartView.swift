e4b2da63b1e06e9656f4b0cbcac716fd2b1b1aa3
d.kanellidis@msensis.com <d.kanellidis@msensis.com>
2022-08-08 11:06:04 +0300

    Commit added Bar Chart

---
+++ WeatherApp/views/BarChartView.swift
@@ -0,0 +1,35 @@
+//
+//  BarChartView.swift
+//  WeatherApp
+//
+//  Created by MSENSIS on 22/7/22.
+//
+
+import SwiftUI
+
+struct BarChartView: View {
+    @Binding var data: [Double]
+
+    
+    var highestData: Double{
+        let max = data.max() ?? 0.0
+        if max == 0 {return 1.0}
+        return max
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+              HStack(alignment: .bottom, spacing: 4.0) {
+                  ForEach(data.indices, id: \.self) { index in
+                      let width = (geometry.size.width / CGFloat(data.count)) - 4.0
+                      let height = geometry.size.height * data[index] / highestData
+                      
+                      BarView(datum: data[index])
+                          .frame(width: width, height: height, alignment: .bottom)
+                  }
+              }
+            }
+    }
+}
+
+
